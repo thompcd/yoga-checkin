2c76561eec13ed7882803a34ceceee796ab5b0b2
Corey Thompson <corey.dwayne.thompson@gmail.com>
2019-08-24 17:01:08 -0500

    attempting to use svelte-calendar

--- src/App.svelte
+++ src/App.svelte
@@ -2,12 +2,78 @@
     import Profile from './Profile.svelte';
     import Todos from './Todos.svelte';
     import TextAnimation from './TextAnimation.svelte';
+    import Datepicker from './Calendar/Datepicker.svelte';
 
     import { auth, googleProvider } from './firebase';
     import { authState } from 'rxfire/auth';
+    import { formatDate } from 'timeUtils'
+    import { onMount } from 'svelte';
 
     let user;
     let startText = "REBEL";
+
+    const today = new Date();
+	let start = new Date();
+	let dateFormat = '#{l}, #{F} #{j}, #{Y}';
+	let noWeekendsSelectableCallback = (date) => date.getDay() !== 0 && date.getDay() !== 6;
+	let formattedSelected;
+	let dateChosen = false;
+	let exampleFormatted = false;
+	let exampleChosen = false;
+	
+	$: end = new Date(start.getTime() + 1000 * 3600 * 24 * 720);
+	
+	let threeDaysInPast;
+	$: {
+	  const date = new Date(today);
+	  date.setDate(date.getDate() - 3);
+	  threeDaysInPast = date;
+	}
+
+	let tomorrow;
+	$: {
+	  const date = new Date(today);
+	  date.setDate(date.getDate() + 1);
+	  tomorrow = date;
+	}
+
+	let inThirtyDays;
+	$: {
+	  const date = new Date(start);
+	  date.setDate(date.getDate() + 30);
+	  inThirtyDays = date;
+	}
+
+	function logChoice(date) {
+	  // eslint-disable-next-line
+	  console.log(`User chose ${date}.`);
+	}
+
+	// onMount(() => {
+	//   // eslint-disable-next-line
+	//   hljs.initHighlightingOnLoad();
+	// });
+
+//     var today = new Date();
+//     console.log(formatDate(today,'Your appointment is on #{l}, #{F} #{m}, #{Y} at #{g}:#{i} #{A}.'));
+
+//     function addDays(date, days) {
+//         var result = new Date(date);
+//         result.setDate(result.getDate() + days);
+//         return result;
+//     }
+
+//     function subtractDays(date, days) {
+//         var result = new Date(date);
+//         result.setDate(result.getDate() - days);
+//         return result;
+// }
+//     var threeDaysInPast = subtractDays(today, 3);
+//     var inThirtyDays = addDays(today, 30);
+//     var noWeekendsSelectableCallback = function(date) {
+//         return true;
+//}
+
 
     let w = window,
     d = document,
@@ -39,11 +105,11 @@ button{
     margin: auto;
 }
 
-
 .profile-section{
     border: 1px solid #888;
     border-radius: 1px;
     box-shadow: 5px 10px #888888;
+    padding: 16px;
 }
 
 .todo-section{
@@ -85,12 +151,20 @@ button{
             <button on:click={ () => auth.signOut() }>Logout</button>
         </div>
         <div class="calendar-section">
-            <div class="calendar"></div>
+            <div class="calendar">
+                <Datepicker
+                    format="{formatDate}"
+                    start={threeDaysInPast}
+                    end={inThirtyDays}
+                    selectableCallback={noWeekendsSelectableCallback}
+                />
+            </div>
         </div>
         <div class="todo-section">
             <Todos uid={user.uid} />
         </div>
 {:else}
+    <!-- TODO: wait for auto login -->
     <div class="bounds-logged-out">
         <div>
             <TextAnimation visible=true drawText={startText}/>
--- src/Profile.svelte
+++ src/Profile.svelte
@@ -2,25 +2,57 @@
     export let displayName;
     export let photoURL;
     export let uid;
+    export let email;
+    export let phoneNumber;
+    // TODO: functional updates
+    export let updateEmail;
+    export let updatePhoneNumber;
+    export let updatePassword;
+    export let updateProfile;
 </script>
 
 <style>
 
-    .profile h3{
-        padding-right: 4px;
+    .profile-img-wrapper{
+        display: inline-flex;
     }
+
+    .profile-img-wrapper h4{
+        padding-left: 8px;
+    }
+
     .profile{
         display: flex;
+        flex-direction: column;
+    }
+
+    .profile li{
+        list-style: none;
     }
 
     .profile-img{
         width: 50px;
-        height: 50px; 
-        padding: 10px;
+        height: 50px;
+    }
+
+    .account-info{
+        padding-left: inherit;
     }
 </style>
 
 <div class="profile">
-    <img class="profile-img" src={ photoURL } alt="user { uid }">
-    <h3>Hi, { displayName }!</h3>
+    <div class="profile-img-wrapper">
+        <img class="profile-img" src={ photoURL } alt="user { uid }"> 
+        <h4>Hi, { displayName }!</h4>
+    </div>
+    <ul class="account-info"><h4>Account Information</h4>
+        <li>Email: {email}</li>
+        <li>Phone: {phoneNumber === null ? "None" : phoneNumber}</li>
+        <br>
+        <li><a href={updateEmail}>Update Email</a></li>
+        <li><a href={updatePhoneNumber}>Update Phone</a></li>
+        <li><a href={updatePassword}>Update Password</a></li>
+        <li><a href={updateProfile}>Update Profile</a></li>
+        
+    </ul>
 </div>
